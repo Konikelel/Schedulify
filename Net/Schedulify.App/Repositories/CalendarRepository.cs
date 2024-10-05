35e592d5ceb37709adf59399a2a7330166bc89bf
Konikelel <wiktoralbinowski@gmail.com>
2024-10-05 17:52:29 +0200

    implement calendar repository

--- Net/Schedulify.App/Repositories/CalendarRepository.cs
+++ Net/Schedulify.App/Repositories/CalendarRepository.cs
@@ -1,4 +1,7 @@
+using Dapper;
 using Schedulify.App.Attributes;
+using Schedulify.App.Database;
+using Schedulify.App.Dtos;
 using Schedulify.App.Entities;
 using Schedulify.App.Enums;
 
@@ -13,44 +16,101 @@ public interface ICalendarRepository
     
     public Task<IEnumerable<CalendarEntity>> GetByOwnerIdAsync(Guid id, CancellationToken token = default);
     
-    public Task<bool> CreateAsync(CalendarEntity calendar, CancellationToken token = default);
+    public Task<bool> CreateAsync(CreateCalendarDto calendar, CancellationToken token = default);
     
-    public Task<bool> UpdateAsync(CalendarEntity calendar, CancellationToken token = default);
+    public Task<bool> UpdateAsync(UpdateCalendarDto calendar, CancellationToken token = default);
     
     public Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
 }
 
 public class CalendarRepository: ICalendarRepository
 {
-    private readonly List<CalendarEntity> _calendar; //TODO: Replace this with a real database
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+    
+    public CalendarRepository(IDbConnectionFactory dbConnectionFactory)
+    {
+        this._dbConnectionFactory = dbConnectionFactory;
+    }
 
     public async Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default)
     {
-        return false;
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+
+        return await connection.ExecuteScalarAsync<bool>(new CommandDefinition(
+            "SELECT COUNT(1) FROM Calendars WHERE Id = @Id",
+            new { Id = id },
+            cancellationToken: token
+        ));
     }
+    
 
     public async Task<CalendarEntity?> GetByIdAsync(Guid id, CancellationToken token = default)
     {
-        return _calendar.SingleOrDefault(x => x.Id == id);
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+
+        return await connection.QuerySingleOrDefaultAsync<CalendarEntity>(new CommandDefinition(
+            "SELECT * FROM Calendars WHERE Id = @Id",
+            new { Id = id },
+            cancellationToken: token
+        ));
     }
     
     public async Task<IEnumerable<CalendarEntity>> GetByOwnerIdAsync(Guid id, CancellationToken token = default)
     {
-        return this._calendar.AsEnumerable();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+
+        return await connection.QueryAsync<CalendarEntity>(new CommandDefinition(
+            "SELECT * FROM Calendars WHERE Id = @Id",
+            new { Id = id },
+            cancellationToken: token
+        ));
     }
 
-    public async Task<bool> CreateAsync(CalendarEntity calendar, CancellationToken token = default)
+    public async Task<bool> CreateAsync(CreateCalendarDto calendar, CancellationToken token = default)
     {
-        return true;
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+        using var transaction = connection.BeginTransaction();
+        
+        var result = await connection.ExecuteAsync(new CommandDefinition(
+            "INSERT INTO Calendars (Id, Name, OwnerId, CreatedAt, UpdatedAt) VALUES (@Id, @Name, @OwnerId, @CreatedAt, @UpdatedAt)",
+            calendar,
+            transaction: transaction,
+            cancellationToken: token
+        ));
+        
+        if (result == 1) transaction.Commit(); else transaction.Rollback();
+        return result == 1;
     }
 
-    public async Task<bool> UpdateAsync(CalendarEntity calendar, CancellationToken token = default)
+    public async Task<bool> UpdateAsync(UpdateCalendarDto calendar, CancellationToken token = default)
     {
-        return true;
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+        using var transaction = connection.BeginTransaction();
+        
+        var result = await connection.ExecuteAsync(new CommandDefinition(
+            $"INSERT INTO Calendars (Id, Name, OwnerId, UpdatedAt) VALUES (@Id, @Name, @OwnerId, @UpdatedAt)",
+            calendar,
+            transaction: transaction,
+            cancellationToken: token
+        ));
+        
+        if (result == 1) transaction.Commit(); else transaction.Rollback();
+        return result == 1;
     }
 
     public async Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
     {
-        return true;
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+        using var transaction = connection.BeginTransaction();
+        
+        var result = await connection.ExecuteAsync(new CommandDefinition(
+            "DELETE FROM Calendars WHERE Id = @Id",
+            new { Id = id },
+            transaction: transaction,
+            cancellationToken: token
+        ));
+        
+        if (result == 1) transaction.Commit(); else transaction.Rollback();
+        return result == 1;
     }
 }
