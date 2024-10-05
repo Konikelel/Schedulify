8bf194a5f640221e2da4bd57cf0dd45ab9efeb52
Konikelel <wiktoralbinowski@gmail.com>
2024-10-05 13:48:17 +0200

    add user dtos and repository

---
+++ Net/Schedulify.App/Dtos/UserDtos.cs
@@ -0,0 +1,18 @@
+namespace Schedulify.App.Dtos;
+
+public abstract class UserBaseDtos
+{
+    public required Guid Id { get; init; }
+    
+    public required string Username { get; set; }
+    
+    public required string Email { get; set; }
+    
+    public string? ImageUrl { get; set; }
+    
+    public required string Password { get; set; }
+}
+
+public class CreateUserDto: UserBaseDtos;
+
+public class UpdateUserDto: UserBaseDtos;
---
+++ Net/Schedulify.App/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+using Schedulify.App.Attributes;
+using Schedulify.App.Entities;
+using Schedulify.App.Enums;
+
+namespace Schedulify.App.Repositories;
+
+[Injectable(InjectableTypeEnum.Singleton, typeof(UserRepository))]
+public interface IUserRepository
+{
+    public Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
+
+    public Task<bool> ExistsByUsernameAsync(string username, CancellationToken token = default);
+
+    public Task<bool> ExistsByEmailAsync(string email, CancellationToken token = default);
+    
+    public Task<UserEntity?> GetByIdAsync(Guid id, CancellationToken token = default);
+
+    public Task<bool> CreateAsync(UserEntity user, CancellationToken token = default);
+    
+    public Task<bool> UpdateAsync(UserEntity user, CancellationToken token = default);
+    
+    public Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
+}
+
+public class UserRepository: IUserRepository
+{
+    private readonly List<UserEntity> _users; //TODO: Replace this with a real database
+
+    public async Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default)
+    {
+        return false;
+    }
+
+    public async Task<bool> ExistsByUsernameAsync(string username, CancellationToken token = default)
+    {
+        return false;
+    }
+    
+    public async Task<bool> ExistsByEmailAsync(string email, CancellationToken token = default)
+    {
+        return false;
+    }
+
+    public async Task<UserEntity?> GetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        return _users.SingleOrDefault(x => x.Id == id);
+    }
+
+    public async Task<bool> CreateAsync(UserEntity user, CancellationToken token = default)
+    {
+        return true;
+    }
+
+    public async Task<bool> UpdateAsync(UserEntity user, CancellationToken token = default)
+    {
+        return true;
+    }
+
+    public async Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
+    {
+        return true;
+    }
+}
