4f60ffd7d5b702770cdcd1a83229aaa357c6de35
Konikelel <wiktoralbinowski@gmail.com>
2024-10-07 13:01:55 +0200

    add simple auth controller and requests

---
+++ Net/Schedulify.Api/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Schedulify.Contracts.Requests;
+
+namespace Schedulify.Api.Controllers;
+
+[ApiController]
+public class AuthController: ControllerBase
+{
+    private readonly IMapper _mapper;
+
+    public AuthController(
+        IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost(ApiEndpoints.Users.Register)]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
+    {
+        throw NotImplementedException();
+    }
+
+    [HttpPost(ApiEndpoints.Users.Login)]
+    public async Task<IActionResult> Login([FromBody] LoginUserRequest request)
+    {
+        throw NotImplementedException();
+    }
+}
--- Net/Schedulify.Contracts/Requests/UserRequests.cs
+++ Net/Schedulify.Contracts/Requests/UserRequests.cs
@@ -1,14 +1,19 @@
 namespace Schedulify.Contracts.Requests;
 
-public abstract class AbstractBaseUserRequest
+public class LoginUserRequest
+{
+    public required string UsernameOrEmail { get; init; }
+    
+    public required string Password { get; init; }
+}
+
+public class RegisterUserRequest
 {
     public required string Username { get; init; }
     
     public required string Email { get; init; }
     
     public string? ImageUrl { get; init; }
+    
+    public required string Password { get; init; }
 }
-
-public class CreateUserRequest : AbstractBaseUserRequest;
-
-public class UpdateUserRequest : AbstractBaseUserRequest;
