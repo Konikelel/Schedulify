29f195c7ded4862a79da7f0c1e2779a80f24acce
Konikelel <wiktoralbinowski@gmail.com>
2024-10-09 21:42:49 +0200

    remove user endpoints

--- Net/Schedulify.Api/ApiEndpoints.cs
+++ Net/Schedulify.Api/ApiEndpoints.cs
@@ -36,12 +36,4 @@ public static class ApiEndpoints
         public const string Update = $"{Base}/{{id:guid}}";
         public const string Delete = $"{Base}/{{id:guid}}";
     }
-
-    public static class Users
-    {
-        private const string Base = $"{ApiBase}/auth";
-        
-        public const string Login = $"{Base}/login";
-        public const string Register = $"{Base}/regiser";
-    }
 }
